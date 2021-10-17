e5641c9344cd98d0068bec8af80f0314c6230311
YuxinZhang <taylorzhangyx@gmail.com>
2021-10-17 15:27:47 +0800

    add append and remove logic
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- mq.go
+++ mq.go
@@ -1,14 +1,12 @@
 package main
 
-import "fmt"
-
 type MsgQue struct {
 	count int32         // number of queues of this message queue
 	bank  []interface{} // the place that saves the data
 }
 
 func (q *MsgQue) AppendOne(v interface{}) error {
-	fmt.Println(v)
+	q.bank = append(q.bank, v)
 	return nil
 }
 
@@ -21,13 +19,15 @@ func (q *MsgQue) Peek() (interface{}, error) {
 }
 
 // Len Show the number of element in this MsgQue object.
-func (q *MsgQue) Len() int64 {
-	return 0
+func (q *MsgQue) Len() int {
+	return len(q.bank)
 }
 
 // ConsumeOne Get and remove the first element from the queue.
 //   Return the element with nil error when element not.
 //   Return nil with error when element is not found or Error occurred.
 func (q *MsgQue) ConsumeOne() (interface{}, error) {
-	return nil, nil
+	e := q.bank[0]
+	q.bank = q.bank[1:]
+	return e, nil
 }
